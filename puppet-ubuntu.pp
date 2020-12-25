55e3c2553ad0e28bcab8e6f35bed9137ad2613f2
AJ6EE <63912806+AJ6EE@users.noreply.github.com>
2020-12-25 11:56:36 -0800

    Create puppet-ubuntu.pp

---
+++ puppet-ubuntu.pp
@@ -0,0 +1,37 @@
+file  { '/etc/YSFReflector.ini':
+	ensure => present,
+	source => '/root/YSFReflector/YSFReflector.ini',
+	mode => '0644',
+}
+
+file { '/var/log/YSFReflector/':
+	ensure => directory,
+	mode => '0755',
+}	
+
+$svcs = [
+'apache2',
+'YSFReflector',
+]
+
+service { $svcs:
+	ensure => running,
+	enable => true,
+}
+
+user { "pi":
+    ensure => present,
+    groups => [www-data],
+}
+
+file  { '/var/www/html/index.html':
+	ensure => absent,
+}
+
+file { "/var/www/html":
+  ensure => directory,
+  recurse => true,
+  owner => "www-data",
+  group => "www-data",
+  mode => '0755', 
+  }
